dd4259815df5f441535ff5909bfcf83c002aee6a
ben2613 <sosgyuki@gmail.com>
2025-09-10 23:23:21 +0800

    show both en jp in splash screen

--- src/components/ui/SplashScreen.tsx
+++ src/components/ui/SplashScreen.tsx
@@ -6,8 +6,10 @@ import styles from './SplashScreen.module.css';
 
 const SplashScreen: React.FC = () => {
   const { t } = useTranslation();
-  const title = t('meta.title');
-  const description = t('meta.description');
+  const titleEN = t('meta.title', { lng: 'en' });
+  const descriptionEN = t('meta.description', { lng: 'en' });
+  const titleJP = t('meta.title', { lng: 'ja' });
+  const descriptionJP = t('meta.description', { lng: 'ja' });
   return (
     <motion.div
       initial={{ opacity: 1, scale: 1 }}
@@ -20,19 +22,25 @@ const SplashScreen: React.FC = () => {
         initial={{ scale: 0.8, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
         transition={{ duration: 0.7, delay: 0.1, type: 'spring' }}
-        className="flex flex-col items-center"
+        className="flex flex-col items-center h-full justify-evenly py-40"
       >
+        <div className="text-center">
+          <h1 className="text-xl md:text-4xl font-bold text-gray-800 mb-2 drop-shadow-lg">{titleEN}</h1>
+          <p className="text-lg text-gray-600 mb-4">{descriptionEN}</p>
+        </div>
         <div className={styles.container}>
           <Image
             src={process.env.NEXT_PUBLIC_BASE_PATH + 'chamapoint.png'}
             className="rounded-full"
             alt="Chama"
-            width={100}
-            height={100}
+            width={200}
+            height={200}
           />
         </div>
-        <h1 className="text-xl md:text-4xl font-bold text-gray-800 mb-2 drop-shadow-lg">{title}</h1>
-        <p className="text-lg text-gray-600 mb-4">{description}</p>
+        <div className="text-center">
+          <h1 className="text-2xl md:text-4xl font-bold text-gray-800 mb-2 drop-shadow-lg">{titleJP}</h1>
+          <p className="text-lg text-gray-600 mb-4">{descriptionJP}</p>
+        </div>
       </motion.div>
     </motion.div>
   );
